c15c43412d197813f314cd9c3462f48c4f4868ba
Murtaza-123 <murtazazaidi420@gmail.com>
2023-11-23 17:32:05 +0500

    fix: add swagger auth decorator

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -9,10 +9,13 @@ import {
 import { TaskService } from './task.service';
 import { TaskDto } from 'src/dto/task.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Post('createPost')
   async createPost(@Body() taskDto: TaskDto) {
@@ -20,6 +23,7 @@ export class TaskController {
     return task;
   }
 
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Get('/getPost')
   async getPost(@Request() req) {
